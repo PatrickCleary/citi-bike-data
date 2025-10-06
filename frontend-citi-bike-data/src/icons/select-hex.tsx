41f95bbf58a8327d425261d70c5b9aea19b63b12
patrickCleary <patrickdaviscleary@gmail.com>
2025-10-06 16:38:36 -0400

    lint2

--- frontend-citi-bike-data/src/icons/select-hex.tsx
+++ frontend-citi-bike-data/src/icons/select-hex.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import PointHex from "./point_hex.svg";
 
 const SelectHexIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
-    
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return <img src={PointHex.src} alt="Hex icon" {...(props as any)} />;
 };
 
