b452057fc88c60a9be30f0083ef60981b9fdddc1
patrickCleary <patrickdaviscleary@gmail.com>
2025-10-13 11:07:17 -0400

    google analytics

--- frontend-citi-bike-data/src/app/layout.tsx
+++ frontend-citi-bike-data/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
+import Script from "next/script";
 
 import "./globals.css";
 import Providers from "./providers";
@@ -29,6 +30,18 @@ export default function RootLayout({
         />
       </head>
       <body className={outfit.className}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CN6RVMTMGV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CN6RVMTMGV');
+          `}
+        </Script>
         <Providers>{children}</Providers>
       </body>
     </html>
